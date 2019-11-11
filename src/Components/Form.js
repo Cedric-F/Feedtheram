2b9705657ea0a90424d2711c62b18ada8c49bcec
Cedric-F <cedric.fontaine@gmx.fr>
2019-11-11 08:31:10 +0100

    Tabs correction

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 class Form extends React.Component {
 
-	render() {
-		const button = (
-			<button className="submit">Je participe</button>);
-		const loader = (<div className="load"></div>);
+  render() {
+    const button = (
+      <button className="submit">Je participe</button>);
+    const loader = (<div className="load"></div>);
 
-		return (
+    return (
       <div className="form">
-  			<form onSubmit={this.props.click}>
-  				<input type="number" min="1" max="100" className="id" onChange={this.props.change} />
-  				{this.props.loading ? loader : button}
-  			</form>
+        <form onSubmit={this.props.click}>
+          <input type="number" min="1" max="100" className="id" onChange={this.props.change} />
+          {this.props.loading ? loader : button}
+        </form>
       </div>
-		)
-	}
+    )
+  }
 }
 
 export default Form;
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,13 +3,13 @@ import {Link} from "react-router-dom";
 import {Navbar} from 'react-bootstrap';
 
 class Nav extends React.Component {
-	render() {
-		return (
-			<Navbar className="head" bg="light" expand="lg">
-			  <Navbar.Brand><Link to="/">Feed the Ram</Link></Navbar.Brand>
-			</Navbar>
-		)
-	}
+  render() {
+    return (
+      <Navbar className="head" bg="light" expand="lg">
+        <Navbar.Brand><Link to="/">Feed the Ram</Link></Navbar.Brand>
+      </Navbar>
+    )
+  }
 }
 
 export default Nav;
